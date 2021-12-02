e413c28bc74d392379ac15087121a6ec6e0c2983
Todd Christensen <toddchristensen@protonmail.com>
2021-12-03 00:42:17 +0800

    adding footer variables that magically went missing

--- components/Footer/FooterBar.component.tsx
+++ components/Footer/FooterBar.component.tsx
@@ -1,5 +1,6 @@
 import CurrentTime from "components/CurrentTime/CurrentTime.component";
 import CurrentWeather from "components/CurrentWeather/CurrentWeather.component";
+import { locale } from "config/locale.config";
 
 export default function FooterBar() {
   return (
@@ -8,8 +9,16 @@ export default function FooterBar() {
       className="text-gray-700 dark:text-gray-300 w-full flex flex-wrap items-center justify-between rounded shadow-md p-1 min-h-[50px] bg-radial from-sky-200 via-sky-300 to-sky-400 dark:from-indigo-700 dark:via-indigo-800 dark:to-indigo-900"
     >
       <div className="bg-white dark:bg-black w-full rounded bg-opacity-80 dark:bg-opacity-20 flex flex-wrap items-center justify-between px-3">
-        <CurrentTime />
-        <CurrentWeather />
+        <CurrentTime
+          city={locale.CURRENT_CITY}
+          countryCode={locale.CURRENT_COUNTRY_CODE}
+          timezone={locale.TIMEZONE}
+        />
+        <CurrentWeather
+          city={locale.CURRENT_CITY}
+          countryCode={locale.CURRENT_COUNTRY_CODE}
+          timezone={locale.TIMEZONE}
+        />
       </div>
     </div>
   );
