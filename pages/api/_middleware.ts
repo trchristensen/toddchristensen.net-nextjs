d06bf0d45a1b0104ac66957cc5c1a0da9b4672f7
Todd Christensen <toddchristensen@protonmail.com>
2021-12-05 14:20:56 +0800

    adding headers to middleware api

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,6 +1,11 @@
-import type { NextRequest } from "next/server";
+import type { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const { nextUrl: url, geo } = req;
-  console.log("GEO LOCATION OBJ ===>", geo)
+export async function middleware(req: NextRequest, res:NextResponse) {
+  const { nextUrl: url, geo, ip } = req;
+
+  res.headers.set('x-user-ip', req.ip)
+  res.headers.set("x-city-set", geo.city);
+  res.headers.set("x-region-set", geo.region);
+  res.headers.set("x-country-set", geo.country);
+  
 }
