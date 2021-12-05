00aa8af4352ed74b2cdcde83b5a33ea171f25598
Todd Christensen <toddchristensen@protonmail.com>
2021-12-05 14:26:10 +0800

    removing custom headers

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -2,10 +2,4 @@ import type { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest, res:NextResponse) {
   const { nextUrl: url, geo, ip } = req;
-
-  res.headers.set('x-user-ip', req.ip)
-  res.headers.set("x-city-set", geo.city);
-  res.headers.set("x-region-set", geo.region);
-  res.headers.set("x-country-set", geo.country);
-  
 }
