08a49ff7aa99a7b65117a7ee083319fb3ab30871
Todd Christensen <toddchristensen@protonmail.com>
2021-12-05 13:29:17 +0800

    added missing types

--- lib/types.ts
+++ lib/types.ts
@@ -85,3 +85,28 @@ export type Book = {
 export type Views = {
   total: number;
 };
+
+
+export type Subscribers = {
+  count: number;
+};
+
+
+
+export type YouTube = {
+  subscriberCount: number;
+  viewCount: number;
+};
+
+export type GitHub = {
+  stars: number;
+};
+
+export type Gumroad = {
+  sales: number;
+};
+
+export type Unsplash = {
+  downloads: number;
+  views: number;
+};
