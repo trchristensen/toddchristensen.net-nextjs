73c9941328bf34b8e17df43a9afab835e89d9bc9
Todd Christensen <toddchristensen@protonmail.com>
2021-12-11 02:17:59 +0800

    fix: type path missing

--- lib/openLibrary.ts
+++ lib/openLibrary.ts
@@ -1,7 +1,8 @@
-import { Book } from "./types"; 
+import { book } from ".prisma/client";
+
 
 export const searchBooks = async (query: string | any = null) => {
-  const books: Book[] | any = await fetch(
+  const books: any | any = await fetch(
     `http://openlibrary.org/search.json?q=${query}&limit=15`
   ).then((res) => res.json());
   return books?.docs.slice(0, 10);
